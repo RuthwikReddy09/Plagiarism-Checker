239700584248c6d4bf60cc5e86c951a3f1d5d739
ruthwikreddy09 <druthwik09@gmail.com>
2023-09-19 20:01:36 +0530

    integrated backend

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,25 @@
 from numpy import vectorize 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from flask import Flask
+from flask import Flask,request,jsonify
+from flask_cors import CORS
+
 app=Flask(__name__) 
+cors = CORS(app)
 
 vectorize = lambda Text: TfidfVectorizer().fit_transform(Text).toarray()
 similarity = lambda doc1, doc2: cosine_similarity([doc1, doc2])
-sample_files=["one","two"]
-vectors = vectorize(["hello this is sathwik","hi this is ruthwik"])
 
-def check_plagiarism():
+def check_plagiarism(t1,t2):
+    vectors = vectorize([t1,t2])
     score = similarity(vectors[0],vectors[1])
     return score[0][1]
 
-@app.route("/")
+@app.route("/",methods=["GET","POST"])
 def home():
-    return str(check_plagiarism())
+    if request.method == "POST":
+        jsonData = request.get_json()
+        return jsonify(check_plagiarism(jsonData['t1'],jsonData['t2']))
 
 if(__name__=="__main__"):
-    app.run()
+    app.run(debug=True)
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,21 +1,40 @@
 import React from "react";
+import { useState } from "react";
 import "../styles/home.css";
 function Home() {
+  const [text1,setText1]=useState("");
+  const [text2,setText2]=useState("");
+  function btnHnadler(){
+    console.log(text1);
+    console.log(text2);
+    fetch("http://127.0.0.1:5000/",{
+      method: 'POST',
+      body: JSON.stringify({t1:text1,t2:text2}),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then((data)=>data.json())
+    .then((res)=>console.log(res))
+    .catch((err)=>{
+      console.warn(err);
+    })
+  }
   return (
     <div className="home">
       <h1 className="heading">Plagiarism Checker</h1>
       <div className="content">
         <div className="text">
           <h4 className="text-heading">Text1</h4>
-          <textarea  name="text1" id="text1" placeholder="Enter Text"/>
+          <textarea  name="text1" id="text1" placeholder="Enter Text" onChange={(e)=>setText1(e.target.value)}/>
         </div>
         <div className="text">
           <h4 className="text-heading">Text2</h4>
-          <textarea  name="text2" id="text2" placeholder="Enter Text"/>
+          <textarea  name="text2" id="text2" placeholder="Enter Text" onChange={(e)=>setText2(e.target.value)}/>
         </div>
       </div>
       <div className="submit">
-        <a href="#" className="btn">Check For Plagiarism</a>
+        <button className="btn" onClick={(e)=>btnHnadler(e)}>Check For Plagiarism</button>
       </div>
     </div>
   );
